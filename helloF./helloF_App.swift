0fdd8339a58c65711c9bfe75d0f56400ee3012d2
Sara Lima <saralima06201234@gmail.com>
2023-02-17 17:25:25 -0300

    Initial Commit

---
+++ helloF./helloF_App.swift
@@ -0,0 +1,17 @@
+//
+//  helloF_App.swift
+//  helloF.
+//
+//  Created by user on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct helloF_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
